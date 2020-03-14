40590cf9306f95d49b3cb414601ebd3731ec5748
dmcbrayer <dmcbrayer@gmail.com>
2020-03-14 15:03:13 -0400

    Adds table to recipes#index

--- lib/bread_web/controllers/recipe_controller.ex
+++ lib/bread_web/controllers/recipe_controller.ex
@@ -7,4 +7,9 @@ defmodule BreadWeb.RecipeController do
     recipes = Recipes.list_recipes()
     render(conn, "index.html", recipes: recipes)
   end
+
+  def show(conn, %{"id" => recipe_id}) do
+    recipe = Recipes.get_recipe!(recipe_id)
+    render(conn, "show.html", recipe: recipe)
+  end
 end
