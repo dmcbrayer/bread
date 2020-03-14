81882f96389c90f0342b816747ea3350c7bc5508
dmcbrayer <dmcbrayer@gmail.com>
2020-03-14 16:32:24 -0400

    Adds a recipes#delete action

--- lib/bread_web/controllers/recipe_controller.ex
+++ lib/bread_web/controllers/recipe_controller.ex
@@ -12,4 +12,14 @@ defmodule BreadWeb.RecipeController do
     recipe = Recipes.get_recipe!(recipe_id)
     render(conn, "show.html", recipe: recipe)
   end
+
+  def delete(conn, %{"id" => recipe_id}) do
+    recipe = Recipes.get_recipe!(recipe_id)
+
+    {:ok, _recipe} = Recipes.delete_recipe(recipe)
+
+    conn
+    |> put_flash(:info, "Recipe deleted successfully.")
+    |> redirect(to: Routes.recipe_path(conn, :index))
+  end
 end
