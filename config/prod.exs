2cc6a2d6da9820779fc73236b71bed9a9b9da9f5
dmcbrayer <dmcbrayer@gmail.com>
2020-03-20 16:53:32 -0400

    I honestly don't know if this made a difference

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,6 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :bread, BreadWeb.Endpoint,
-  url: [host: "bread.roundtabledev.com", port: 4000],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
