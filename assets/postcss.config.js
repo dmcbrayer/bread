33f9e9b6d010f4bd4bd7577b628a9c15690c57ee
dmcbrayer <dmcbrayer@gmail.com>
2020-04-01 09:52:36 -0400

    Finally gets purgeCSS working in production
    This was a weird one.  By default the NODE_ENV is set by webpack's
    --mode production option.  Unfortunately, the postcss config does not
    by default respect the process.env set by webpack's mode option.
    
    More infor here: https://github.com/postcss/postcss-loader/issues/368#issuecomment-518492725

--- assets/postcss.config.js
+++ assets/postcss.config.js
@@ -11,9 +11,7 @@ module.exports = {
   plugins: [
     require('tailwindcss'),
     require('autoprefixer'),
-    ...process.env.NODE_ENV === "production"
-      ? [purgecss]
-      : []
+    ...process.env.NODE_ENV === 'production' ? [purgecss] : []
   ]
 }
 
