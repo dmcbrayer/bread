821863d999e42de945b9008faad8815a99a37c6b
dmcbrayer <dmcbrayer@gmail.com>
2020-04-02 09:45:32 -0400

    Adds tailwind config with a new color

---
+++ assets/tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        robin: {
+          100: '#E6FAFA',
+          200: '#BFF3F2',
+          300: '#99EBE9',
+          400: '#4DDDD9',
+          500: '#00CEC9',
+          600: '#00B9B5',
+          700: '#007C79',
+          800: '#005D5A',
+          900: '#003E3C',
+        }
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
