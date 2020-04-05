9170ff870502038c4beb70223c529718960f4716
dmcbrayer <dmcbrayer@gmail.com>
2020-04-05 16:25:37 -0400

    Starts the tracker process when a user joins a channel

--- lib/bread/tracker.ex
+++ lib/bread/tracker.ex
@@ -1,19 +1,38 @@
 defmodule Bread.Tracker do
   use Agent
 
+  @doc """
+  Starts a new tracker map for a user
+  """
   def start_link(initial_value) do
-    Agent.start_link(fn -> initial_value end, name: __MODULE__)
+    Agent.start_link(fn -> initial_value end)
   end
 
-  def value do
-    Agent.get(__MODULE__, fn v -> v end)
+  def bucket_state(bucket) do
+    Agent.get(bucket, fn state -> state end)
   end
 
-  def increment do
-    Agent.update(__MODULE__, fn v -> v + 1 end)
+  @doc """
+  Gets a value from the bucket by key
+  """
+  def get(bucket, key) do
+    Agent.get(bucket, fn state ->
+      Map.get(state, key)
+    end)
   end
 
-  def decrement do
-    Agent.update(__MODULE__, fn v -> v - 1 end)
+  @doc """
+  Puts the `value` for the given `key` in the `bucket`.
+  """
+  def put(bucket, key, value) do
+    Agent.update(bucket, fn state ->
+      Map.put(state, key, value)
+    end)
+  end
+
+  def tick(bucket) do
+    Agent.update(bucket, fn state ->
+      Map.put(state, :time, state.time + 1)
+    end)
   end
 end
--- lib/bread_web/channels/room_channel.ex
+++ lib/bread_web/channels/room_channel.ex
@@ -2,7 +2,33 @@ defmodule BreadWeb.RoomChannel do
   use BreadWeb, :channel
 
   def join("room:" <> room_id, %{"path" => path}, socket) do
-    Bread.Tracker.start_link(%{path: path, user_id: socket.assigns.user_id})
-    {:ok, assign(socket, :room_id, String.to_integer(room_id))}
+    {:ok, ref} =
+      Bread.Tracker.start_link(%{
+        path: path,
+        user_id: socket.assigns.user_id,
+        time: 0
+      })
+
+    socket =
+      socket
+      |> assign(:room_id, String.to_integer(room_id))
+      |> assign(:tracker_ref, ref)
+
+    Process.send_after(self(), :tick, 1000)
+
+    {:ok, socket}
+  end
+
+  def handle_info(:tick, %{assigns: %{tracker_ref: ref}} = socket) do
+    Bread.Tracker.tick(ref)
+    Process.send_after(self(), :tick, 1000)
+
+    {:noreply, socket}
+  end
+
+  # Dump the state of the tracking bucket to something
+  # more permanent
+  def terminate(_reason, %{assigns: %{tracker_ref: ref}} = socket) do
+    IO.inspect(Bread.Tracker.bucket_state(ref))
   end
 end
