3ad62bf73665dda923bf74093ff85830583a7aeb
dmcbrayer <dmcbrayer@gmail.com>
2020-04-08 22:55:40 -0400

    Adds some supervision to all this
    All UserTracker processes are registered with the Analytics.Registry
    so that we can access a global list of all of the UserTracker processes.
    
    These processes are also managed by a DynamicSupervisor so that they
    are not linked to the channel process that started them.
    
    The idea is that these processes should live on when the user navigates
    away from their current page, and even arguably after they leave the
    site.  It should stay alive for long enough to still be there if they
    happen to come back within a reasonable amount of time (about 1 hour).

--- lib/bread_web/channels/room_channel.ex
+++ lib/bread_web/channels/room_channel.ex
@@ -1,16 +1,19 @@
 defmodule BreadWeb.RoomChannel do
   use BreadWeb, :channel
+  alias Analytics.{PageView, UserTracker}
 
-  def join("room:" <> room_id, %{"path" => path}, socket) do
-    {:ok, ref} =
-      Bread.UserTracker.start_link(%{
-        path: path,
-        user_id: socket.assigns.user_id,
-        session_id: socket.assigns.user_id,
-        starts_at: DateTime.utc_now(),
-        ends_at: DateTime.utc_now(),
-        time: 0
-      })
+  def join("room:" <> room_id, %{"path" => path, "sessionId" => session_id}, socket) do
+    user_id = socket.assigns.user_id
+    page_view = PageView.new(user_id, session_id, path)
+    registry_key = PageView.registry_key(page_view)
+
+    ref = case Registry.lookup(Analytics.Registry, registry_key) do
+      [{ref, _}] -> ref
+      [] ->
+        {:ok, ref} =
+          DynamicSupervisor.start_child(Analytics.DynamicSupervisor, {UserTracker, page_view})
+        ref
+    end
 
     socket =
       socket
@@ -23,7 +26,7 @@ defmodule BreadWeb.RoomChannel do
   end
 
   def handle_info(:tick, %{assigns: %{tracker_ref: ref}} = socket) do
-    Bread.UserTracker.tick(ref)
+    UserTracker.tick(ref)
     Process.send_after(self(), :tick, 1000)
 
     {:noreply, socket}
@@ -32,8 +35,8 @@ defmodule BreadWeb.RoomChannel do
   # Dump the state of the tracking bucket to something
   # more permanent
   def terminate(_reason, %{assigns: %{tracker_ref: ref}}) do
-    state = Bread.UserTracker.get_state(ref)
-    IO.puts("Dumping user state to disk...")
-    Task.start(fn -> Bread.Dump.dump(state) end)
+    # state = UserTracker.get_state(ref)
+    # IO.puts("Dumping user state to disk...")
+    # Task.start(fn -> Bread.Dump.dump(state) end)
   end
 end
